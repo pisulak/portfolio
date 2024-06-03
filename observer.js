8c09bebf094c5adbf4d5d82cd94cb5fc70016a5c
Kacper Pisula <127031073+pisulak@users.noreply.github.com>
2024-06-03 20:27:28 +0200

    fixed bugs and finished animations on main

--- observer.js
+++ observer.js
@@ -12,16 +12,16 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll(".education-container-item");
 hiddenElements.forEach((el) => observer.observe(el));
 
-const observer2 = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    console.log(entry);
-    if (entry.isIntersecting) {
-      entry.target.classList.add("project-link-show");
+const observer2 = new IntersectionObserver((entries2) => {
+  entries2.forEach((entry2) => {
+    console.log(entry2);
+    if (entry2.isIntersecting) {
+      entry2.target.classList.add("project-link-show");
     } else {
-      entry.target.classList.remove("project-link-show");
+      entry2.target.classList.remove("project-link-show");
     }
   });
 });
 
 const hiddenElements2 = document.querySelectorAll(".project-link");
-hiddenElements.forEach((el) => observer.observe(el));
+hiddenElements2.forEach((el2) => observer2.observe(el2));
